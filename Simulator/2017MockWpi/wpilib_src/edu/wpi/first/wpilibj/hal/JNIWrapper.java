c0a0e2142a1508156bf7a43f7630b727ec39abbf
PJ Reiniger <pj.reiniger@gmail.com>
2017-05-03 22:39:24 -0400

    Starting to get C++ simulator working.
    Checking in SC update

--- Simulator/2017MockWpi/wpilib_src/edu/wpi/first/wpilibj/hal/JNIWrapper.java
+++ Simulator/2017MockWpi/wpilib_src/edu/wpi/first/wpilibj/hal/JNIWrapper.java
@@ -8,6 +8,12 @@
 package edu.wpi.first.wpilibj.hal;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTablesJNI;
 
 //
 // base class for all JNI wrappers
@@ -19,19 +25,71 @@ public class JNIWrapper
 
     static
     {
-        try
+        if (!libraryLoaded)
         {
-            if (!libraryLoaded)
+            try
             {
-                // System.loadLibrary("wpilibJavaJNI");
-                libraryLoaded = true;
+                System.loadLibrary("ntcore");
             }
+            catch (UnsatisfiedLinkError e)
+            {
+                try
+                {
+                    String osname = System.getProperty("os.name");
+                    String resname;
+                    if (osname.startsWith("Windows"))
+                        resname = "/Windows/" + System.getProperty("os.arch") + "/";
+                    else
+                        resname = "/" + osname + "/" + System.getProperty("os.arch") + "/";
+                    System.out.println("platform: " + resname);
+                    if (osname.startsWith("Windows"))
+                        resname += "snobotSimHal.dll";
+                    else if (osname.startsWith("Mac"))
+                        resname += "libntcore.dylib";
+                    else
+                        resname += "libntcore.so";
+                    InputStream is = NetworkTablesJNI.class.getResourceAsStream(resname);
+                    if (is != null)
+                    {
+                        // create temporary file
+                        if (System.getProperty("os.name").startsWith("Windows"))
+                            jniLibrary = File.createTempFile("NetworkTablesJNI", ".dll");
+                        else if (System.getProperty("os.name").startsWith("Mac"))
+                            jniLibrary = File.createTempFile("libNetworkTablesJNI", ".dylib");
+                        else
+                            jniLibrary = File.createTempFile("libNetworkTablesJNI", ".so");
+                        // flag for delete on exit
+                        jniLibrary.deleteOnExit();
+                        OutputStream os = new FileOutputStream(jniLibrary);
 
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-            System.exit(1);
+                        byte[] buffer = new byte[1024];
+                        int readBytes;
+                        try
+                        {
+                            while ((readBytes = is.read(buffer)) != -1)
+                            {
+                                os.write(buffer, 0, readBytes);
+                            }
+                        }
+                        finally
+                        {
+                            os.close();
+                            is.close();
+                        }
+                        System.load(jniLibrary.getAbsolutePath());
+                    }
+                    else
+                    {
+                        System.loadLibrary("ntcore");
+                    }
+                }
+                catch (IOException ex)
+                {
+                    ex.printStackTrace();
+                    System.exit(1);
+            }
+            }
+            libraryLoaded = true;
         }
     }
 
